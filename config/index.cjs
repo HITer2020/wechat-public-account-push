2f85be7530f3b2ba0780f432a3da4b2f57338995
HITer2020 <72971815+HITer2020@users.noreply.github.com>
2022-10-23 22:45:33 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ol-K76W4jH02UdGPwLXdjbWA7ZgE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'yWFlh_MrEvrxQ_DL5gtNUyMHZyFd8W3yg9RfxzXuaJw',
+      useTemplateId: 'N-oYMNfP9aMyJcx5fDqeSWqMbWTvU8sUw6TJV-cftoQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
